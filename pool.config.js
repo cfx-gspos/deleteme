b084a46795582485adddd32d1702c95856b70cdf
frank <1725325128@qq.com>
2022-03-04 11:50:14 +0800

    sync

---
+++ pool.config.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+  defaultLang: 'zh',
+  testnet: {
+    poolManagerAddress: 'cfxtest:xxxxxxxxxxxxxxxxxxxxxxxxxx',
+    RPC: 'https://test.confluxrpc.com',
+    networkId: 1
+  },
+  mainnet: {
+	  poolManagerAddress: 'cfx:acan8rz12ejvn2wkne4r55eza2mc2hnbh6p60wk6zw',
+    RPC: 'https://main.confluxrpc.com',
+    networkId: 1029
+  }
+}
